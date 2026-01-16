57b1d540278b5028095647c90f3f485346d72e55
Hassan El-hag <ehassan@sacm.org>
2026-01-16 14:45:52 -0500

    edit delete

--- todo_main/views.py
+++ todo_main/views.py
@@ -6,8 +6,10 @@ from todos.models import Task
 
 def home(request):
       tasks = Task.objects.filter(is_completed=False).order_by('-created_at')
+      completed_tasks = Task.objects.filter(is_completed=True)
       context = {
             'tasks': tasks,
+            'completed_tasks': completed_tasks,
             }
 
       return render(request, 'home.html', context )
--- todos/urls.py
+++ todos/urls.py
@@ -5,5 +5,9 @@ from  . import views
 urlpatterns = [
    
     path('addTask', views.addTask, name='addTaskname' ),
+    path('mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done_name' ),
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone_name' ),
+    path('edit_task/<int:pk>', views.edit_task, name='edit_task_name' ),
+     path('delete_task/<int:pk>', views.delete_task, name='delete_task_name' ),
  
 ]
